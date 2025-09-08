920e9f2ec7a9ce01093964fc8d4db1f3a02fc6ba
Madhu Pandey <pandeymadhu80@gmail.com>
2025-09-08 21:53:24 +0530

    readme modified

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -90,8 +90,7 @@ const Contact = () => {
           <br />
           <h3 className="head-text">Let's talk</h3>
           <p className="text-lg text-white-600 mt-3">
-            Whether you’re looking to build a new website, improve your existing platform, or bring a unique project to
-            life, I’m here to help.
+            Looking to collaborate, discuss an idea, or bring your vision to life? I’d love to hear from you.
           </p>
 
           <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
